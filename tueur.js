3fa5d6a0c48946027996d7f711f3ef20001f09c3
NoeBrennetot <146921732+NoeBrennetot@users.noreply.github.com>
2023-10-08 22:14:15 +0200

    Add files via upload

---
+++ tueur.js
@@ -0,0 +1,13 @@
+"use strict"
+
+class Personnage {
+    
+    constructor(nom, pointsDeVie, probabiliteAttaque, probabiliteContreAttaque, degats) {
+      this.nom = nom; 
+      this.pointsDeVie = pointsDeVie; 
+      this.probabiliteAttaque = probabiliteAttaque; 
+      this.probabiliteContreAttaque = probabiliteContreAttaque; 
+      this.degats = degats; 
+    }
+
+// je n'ai pas eu le temps de finir à temps 
